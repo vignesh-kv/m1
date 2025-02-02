07134e06eb5761e7fb2e5a68ae9c88d9c61438dd
vignesh-kv <kv.vignesh2004@gmail.com>
2025-02-02 20:35:05 +0530

    Add files via upload

---
+++ mul.java
@@ -0,0 +1,13 @@
+class mul{
+public static void main(String[]args){
+System.out.println("Enter the number:");
+int n=5;
+int i=1;
+System.out.println("the multiplication table of 5");
+while(i<=10)
+{
+System.out.print(n+"*"+i+"="+(n*i));
+break;
+}
+}
+}
