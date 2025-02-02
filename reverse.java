3dc09c406bb73b1a5313284e1abbd63093075ac6
vignesh-kv <kv.vignesh2004@gmail.com>
2025-02-02 20:34:31 +0530

    Add files via upload

---
+++ reverse.java
@@ -0,0 +1,12 @@
+class reverse{
+public static void main(String[]args){
+int number=1234,reverse=0;
+while(number!=0)
+{
+int remainder=number%10;
+reverse=reverse*10+remainder;
+number=number/10;
+}
+System.out.print("4321");
+}
+}
