260f89c0980d7ae1410882f85c0b4f3d1ed702e2
vignesh-kv <kv.vignesh2004@gmail.com>
2025-03-16 19:48:10 +0530

    Add files via upload

---
+++ five.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+class five{
+public static void main(String[]args){
+System.out.print("Enter the integer:");
+for (int i = 10; i >= 1; i--) {
+System.out.println(i);
+}
+}
+}
