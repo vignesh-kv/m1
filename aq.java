7e2efe2d7935aafb261e02464fff6e30eccbfd57
vignesh-kv <kv.vignesh2004@gmail.com>
2025-03-16 19:47:39 +0530

    Add files via upload

---
+++ aq.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+class aq{
+public static void main(String[]args){
+Scanner scan=new Scanner(System.in);
+Boolean i=false;
+String result = i?"take a seat":"enjoy the light";
+System.out.printf(result);
+}
+} 
